b488bf4ed043c705655dc98f8f25e50f31612dff
Same Bernstein <samebernstein@gmail.com>
2017-11-25 23:36:21 -0500

    Drawing works now

--- create_metro_graph.py
+++ create_metro_graph.py
@@ -40,13 +40,17 @@ G = nx.DiGraph()
 # Create a digraph with the nodes from the node file
 nodes_file_name = "nodes.txt"
 nodes_file = open(nodes_file_name, "r")
-G.add_nodes_from([MetroNode(line.strip()) for line in nodes_file])
+
+for line in nodes_file:
+    G.add_node(line.strip())
+
+# G.add_nodes_from(['Paulina','Clinton','Kedzie'])
 
 # Print the nodes
 # for node in G.nodes:
-#     print(node)
+#     print(node, end = ", ")
 
-lines_files = ['brown_line.txt']#, 'blue.txt']
+lines_files = ['blue.txt', 'brown.txt', 'green.txt', 'orange.txt', 'pink.txt', 'purple.txt', 'yellow.txt', 'red.txt']
 delimiter = ","
 
 for metro_line in lines_files:
@@ -55,25 +59,54 @@ for metro_line in lines_files:
     elist = []
 
     for txt_line in curr_file:
+        # print("txt_line before = \n\n", str(txt_line))
 
-        map(str.strip, txt_line.split(delimiter)) 
+        txt_line = txt_line.split(delimiter)
+
+        for i in range(len(txt_line)):
+            txt_line[i] = txt_line[i].strip()
+
+        # print("txt_line after = \n\n", str(txt_line))
 
         # first direction of edge
-        elist.extend([(txt_line[i], txt_line[i+1], 0) for i in range(len(txt_line) - 1)])
+        elist.extend([(txt_line[i], txt_line[i+1]) for i in range(len(txt_line) - 1)])
         # second direction of edge
-        elist.extend([(txt_line[i+1], txt_line[i], 0) for i in range(len(txt_line) - 1)])
+        elist.extend([(txt_line[i+1], txt_line[i]) for i in range(len(txt_line) - 1)])
 
 
-    G.add_weighted_edges_from(elist)
+    G.add_edges_from(elist)
 
 
-for e in G.edges_iter():
-    print(str(e))
+
+#Print the edges to terminal
+# for e in G.edges:
+#     print(e)
+
+# Drawing the graph
+options = {
+        'node_color': 'r',
+        'node_size': 30,
+        'width': 2,
+        'with_labels': True,
+        'nodecolor' :'r',
+        'arrows' : True,
+        'font_size' : 10,
+        'font_color' : 'black',
+        'linewidths' : 0.5,
+     }
+
+
+pos = nx.spring_layout(G, k=0.05, iterations=20, scale = 2.0)
+
 # plt.subplot(121)
-# nx.draw(G)
+# nx.draw(G, with_labels=True, font_weight='bold')
+
+nx.draw(G, pos, **options)
+
+
 # plt.subplot(122)
 # nx.draw(G, pos=nx.circular_layout(G), nodecolor='r', edge_color='b')
-# plt.show()
+plt.show()
 
 
 
