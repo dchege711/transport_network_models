959bd93ce3f57864b86984407107fa1d5c47f22c
dchege711 <d.chege711@gmail.com>
2017-12-08 22:46:49 -0500

    Document create_metro_graph.py

--- create_metro_graph.py
+++ create_metro_graph.py
@@ -1,66 +1,50 @@
+"""
+Creates a directed graph with train stations as nodes (and with lat-lng attributes).
+Adds unweighted edges between stations that have a direct connection to each other.
+Stores the resulting graph in binary format for subsequent processing.
 
+"""
 
 import networkx as nx
 import matplotlib.pyplot as plt
 from metro_parts import *
-
 import pickle
 
 
 # the digraph of the metro system
 G = nx.DiGraph()
 
-# Create a digraph with the nodes from the node file
-nodes_file_name = "nodes_with_latlng.txt"
-nodes_file = open(nodes_file_name, "r")
-
-name_index = 0
-lat_index = 1
-lng_index = 2
-
-for line in nodes_file:
+# Create a digraph with the nodes from the file containing stations and lat-lng
+for line in open("nodes_with_latlng.txt", "r"):
+    # Each line follows the pattern "station_name    latitude    longitude"
     station_details = line.strip().split(",")
-    lat = float(station_details[lat_index])
-    lng = float(station_details[lng_index])
-    G.add_node(station_details[name_index], pos=(lng, lat))
+    lat = float(station_details[1])
+    lng = float(station_details[2])
+    G.add_node(station_details[0], pos=(lng, lat))
 
-# G.add_nodes_from(['Paulina','Clinton','Kedzie'])
 
-# Print the nodes
-# for node in G.nodes:
-#     print(node, end = ", ")
+# These files were manually compiled. They have station names on a given line
+lines_files = [
+    'blue.txt', 'brown.txt', 'green.txt', 'orange.txt', 
+    'pink.txt', 'purple.txt', 'yellow.txt', 'red.txt'
+]
 
-lines_files = ['blue.txt', 'brown.txt', 'green.txt', 'orange.txt', 'pink.txt', 'purple.txt', 'yellow.txt', 'red.txt']
-delimiter = ","
-
+# For each pair of adjacent stations, create two links to denote the bidirectional 
+# nature of railway stations
 for metro_line in lines_files:
-
     curr_file = open(metro_line, "r")
     elist = []
-
     for txt_line in curr_file:
-        # print("txt_line before = \n\n", str(txt_line))
-
-        txt_line = txt_line.split(delimiter)
-
+        txt_line = txt_line.split(",")
         for i in range(len(txt_line)):
             txt_line[i] = txt_line[i].strip()
-
-        # print("txt_line after = \n\n", str(txt_line))
-
         # first direction of edge
         elist.extend([(txt_line[i], txt_line[i+1]) for i in range(len(txt_line) - 1)])
         # second direction of edge
         elist.extend([(txt_line[i+1], txt_line[i]) for i in range(len(txt_line) - 1)])
-
-
     G.add_edges_from(elist)
 
-
+# Store the graph as a pickle file for later references
 output_file = open(graph_pickle_file_name,'wb')
 pickle.dump(G, output_file)
 output_file.close()
-
-#Print the edges to terminal
-# for e in G.edges:
-#     print(e)
