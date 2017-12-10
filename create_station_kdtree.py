b579c7f16e4124aba41e4dc1638d667046661fa0
Same Bernstein <samebernstein@gmail.com>
2017-12-10 14:07:33 -0500

    Kd tree method works now, create_statoin_kdtree.py now obsolete

--- create_station_kdtree.py
+++ create_station_kdtree.py
@@ -12,14 +12,18 @@ import utilities as ut
 
 # Create an empty tree of 2 dimensions for lat,long
 tree = kdtree.create(dimensions=2)
-stations_file = open(ut.get_path("nodes_with_latlng.txt"), "r")
+stations_file = open(ut.get_path("nodes_with_latlng_updated.txt"), "r")
 
 # Read each station into the kdtree
 for line in stations_file:
-     line = line.split(",")
-     name = line[0]
-     coords = (float(line[1]), float(line[2]))
-     tree.add(metro_parts.Station(name, coords))    # Station is a class in metro_parts
+
+    station_details = line.strip().split("\t")
+    lat_lng = station_details[1].split(",")
+    lat = float(lat_lng[0])
+    lng = float(lat_lng[1])
+    coords = (lat, lng)
+    name = station_details[0]
+    tree.add(metro_parts.Station(name, coords))    # Station is a class in metro_parts
 
 tree = tree.rebalance()
 print("Created a", tree.dimensions, "- d tree")
--- kd_methods.py
+++ kd_methods.py
@@ -12,15 +12,31 @@ without caring whether we have a k-d tree under the hood or not.
 """
 import pickle
 import metro_parts
+import kdtree
+import utilities as ut
 
 class KdTree():
 
+    # creates the k-d tree from the nodes lat/long file
     def __init__(self):
-        # Read in the metro graph G from the pickle file
-        print(metro_parts.station_kdtree_file_name)
-        input_file = open(metro_parts.station_kdtree_file_name,'rb')
-        self.tree = pickle.load(input_file)
-        input_file.close()
+
+        # Create an empty tree of 2 dimensions for lat,long
+        self.tree = kdtree.create(dimensions=2)
+        stations_file = open(ut.get_path("nodes_with_latlng_updated.txt"), "r")
+
+        # Read each station into the kdtree
+        for line in stations_file:
+
+            station_details = line.strip().split("\t")
+            lat_lng = station_details[1].split(",")
+            lat = float(lat_lng[0])
+            lng = float(lat_lng[1])
+            coords = (lat, lng)
+            name = station_details[0]
+            self.tree.add(metro_parts.Station(name, coords))    # Station is a class in metro_parts
+
+        self.tree = self.tree.rebalance()
+        print("Created the", self.tree.dimensions, "-d tree")
 
     # returns tuple of (station name, coords, dist) for the nearest station
     def nearest(self, lat, lon):
@@ -34,14 +50,14 @@ class KdTree():
 def test_methods():
     t = KdTree()
 
-    test_node = (1,2)
-    res = t.nearest(test_node)
-
+    test = (1,2)
+    res = t.nearest(test[0], test[1])
+    print("Nearest node to " + str(test) + " is:")
     print(res)
     print()
-    print("Nearest node to " + str(test_node) + " is:")
+
     # Find the nearest node to the location (1, 2, 3)
-    print(tree.search_nn( test_node )[0].data.name)
+    # print(tree.search_nn(test[0], test[1] )[0].data.name)
 
 if __name__ == "__main__":
     test_methods()
