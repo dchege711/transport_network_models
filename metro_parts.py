ffab79f9ab0823f64794b3339d6e8c51d3434536
dchege711 <d.chege711@gmail.com>
2018-01-12 12:33:07 -0500

    Add a distance_to method in the Station object

--- metro_parts.py
+++ metro_parts.py
@@ -8,6 +8,7 @@ Encapsulates classes for objects that the find relevant. Currently has:
     Station     : Details about a specific station (node) in the network
 """
 import utilities as ut
+from math import asin, sin, cos, sqrt, pi
 
 graph_pickle_file_name = ut.get_path("metro_graph.pkl")
 station_kdtree_file_name = ut.get_path("station_kdtree.pkl")
@@ -47,6 +48,10 @@ class Station():
     def __init__(self, name, coords):
         self.name = name
         self.coords = coords # tuple (lat, long)
+        self.lat = coords[0]
+        self.lng = coords[1]
+        self.lat_in_radians = coords[0] * pi / 180
+        self.lng_in_radians = coords[1] * pi / 180
 
     def __getitem__(self, key):
         return self.coords[key]
@@ -59,6 +64,31 @@ class Station():
       
     def __repr__(self):
         return " ".join([
-            "Name:", self.name, "lat: ", str(self.coords[0]),
-            "lng: ", str(self.coords[1])
+            "Name:", self.name, "lat: ", str(self.lat),
+            "lng: ", str(self.lng)
         ])
+        
+    def distance_to(self, other, units="km"):
+        # https://en.wikipedia.org/wiki/Haversine_formula
+        
+        if units == "km":
+            radius = 6363.97
+        elif units == "miles":
+            radius = 3954.387626
+        else:
+            raise ValueError("{0} units are not supported".format(units))
+        
+        hav_lat = (1 - cos(other.lat_in_radians - self.lat_in_radians)) / 2.0
+        hav_lng = (1 - cos(other.lng_in_radians - self.lng_in_radians)) / 2.0
+        cos_lats = cos(self.lat_in_radians) * cos(other.lat_in_radians)
+        
+        return 2 * radius * asin(sqrt(hav_lat + cos_lats * hav_lng))
+        
+        
+def station_main():
+    station_a = Station("A", [41.953508, -87.746416])
+    station_b = Station("B", [41.947983, -87.744942])
+    assert("{0:.4f}".format(station_a.distance_to(station_b)) == "0.6256")
+    
+if __name__ == "__main__":
+    station_main()
